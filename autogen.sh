23729681de726bd73ab9ecae8a9e227aec3c0c4e
Brian Bidulock <bidulock@openss7.org>
2013-12-26 19:23:36 -0700

    avoid unnecessary commits

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-VERSION=`git describe --always | sed 's|-|.|g;s|[.]g[a-f0-9]*$||'`
+VERSION=
 
-sed -r \
-    -i configure.ac \
-    -e "/^AC_INIT/s|[0-9][0-9]*([.][0-9]*)*|$VERSION|" \
-    -e "/^AC_REVISION/s|[0-9][0-9]*([.][0-9]*)*|$VERSION|"
+if [ -z "$VERSION" ]; then
+	VERSION='1.1'
+	if [ -x "`which git 2>/dev/null`" -a -d .git ]; then
+		VERSION=$(git describe --tags|sed 's,[-_],.,g;s,\.g.*$,,')
+		(
+		   echo -e "# created with git log --stat=76 | fmt -sct -w80\n"
+		   git log --stat=76 | fmt -sct -w80
+		)>ChangeLog
+	fi
+fi
+
+sed -r -e "s:[[](xde-[^]]*)[]],[[][^]]*[]]:[\1],[$VERSION]:
+	   s:AC_REVISION([[][^]]*[]]):AC_REVISION([$VERSION]):" \
+	  configure.template >configure.ac
 
 autoreconf -fiv
